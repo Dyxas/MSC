35bebcd54b769e8e1c4bf494564fe58dcb265efc
blaspin <daniilschmakov@yandex.ru>
2021-12-03 01:16:54 +0500

    1

--- Assets/scripts/RotateItems.cs
+++ Assets/scripts/RotateItems.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class RotateItems : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float oneRotateDeg = 30f;
+    public float oneRotateDeg = 45f;
     public float maxRotateDeg = 0;
     public int maxRotateVal = 0;
+    public bool x = false;
+    public bool y = false;
+    public bool z = false;
+    public Vector3 defaultVector;
 
-    private int steps;
-    private int currentStep = 0;
     private int currentRotateVal = 0;
+    private Vector3 settedAxis;
+    private Vector3 settedReverseAxis;
+    private float settedEulerAxis;
     private bool isRotatingAlways = false;
     
     void Start()
@@ -20,13 +25,25 @@ public class RotateItems : MonoBehaviour
         {
             isRotatingAlways = true;
         }
-
-        if (maxRotateDeg < oneRotateDeg && !isRotatingAlways)
+        if(x)
         {
-            Debug.LogError("Max rotate is lower than one rotate");
-            UnityEditor.EditorApplication.isPlaying = false;
+            float reverse = oneRotateDeg * -1;
+            settedAxis = new Vector3(oneRotateDeg, 0, 0);
+            settedReverseAxis = new Vector3(reverse, 0, 0);
         }
-        steps = 360 / (int)oneRotateDeg;
+        if(y)
+        {
+            float reverse = oneRotateDeg * -1;
+            settedAxis = new Vector3(0, oneRotateDeg, 0);
+            settedReverseAxis = new Vector3(0, reverse, 0);
+        }
+        if(z)
+        {
+            float reverse = oneRotateDeg * -1;
+            settedAxis = new Vector3(0, 0, oneRotateDeg);
+            settedReverseAxis = new Vector3(0, 0, reverse);
+        }
+
     }
 
     
@@ -34,55 +51,82 @@ public class RotateItems : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (x)
+        {
+            settedEulerAxis = transform.eulerAngles.x;
+        }
+        if (y)
+        {
+            settedEulerAxis = transform.eulerAngles.y;
+        }
+        if (z)
+        {
+            settedEulerAxis = transform.eulerAngles.z;
+        }
         //Quaternion.Euler!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         var mw = Input.GetAxis("Mouse ScrollWheel");
+        Item item;
+
+        if(Input.GetKeyUp(KeyCode.R))
+        {
+            transform.eulerAngles = defaultVector;
+            currentRotateVal = 0;
+        }
+
         if (mw > 0)
         {
-            Debug.Log(isRotatingAlways);
             if (isRotatingAlways)
             {
-                transform.Rotate(0, oneRotateDeg, 0, Space.World);
-                Debug.Log(transform.rotation.z);
+                transform.Rotate(settedAxis, Space.World);
             }
             else if (maxRotateDeg != 0)
             {
-                
-                if (maxRotateDeg < 180)
-                    if (transform.rotation.z < maxRotateDeg)
-                        transform.Rotate(0, 0, oneRotateDeg, Space.World);
-                    else { }
-
-                    if (transform.rotation.z < (maxRotateDeg % 180 - 180))
-                        transform.Rotate(0, 0, oneRotateDeg, Space.World);
+                Debug.Log(settedEulerAxis);
+                if (settedEulerAxis < maxRotateDeg)
+                    transform.Rotate(settedAxis, Space.World);
             }
             else if (maxRotateVal != 0 && maxRotateVal > currentRotateVal)
             {
-                
-                if(currentStep < steps)
+                if (Mathf.Round(settedEulerAxis) < 360 - oneRotateDeg)
                 {
-                    currentStep++;
-                    transform.Rotate(0, 0, oneRotateDeg);
+                    transform.Rotate(settedAxis, Space.World);
                 }
-                else
+                else if(currentRotateVal < maxRotateVal)
                 {
                     currentRotateVal++;
-                    currentStep = 0;
+                    transform.Rotate(settedAxis, Space.World);
                 }
+
             }
         }
         else if(mw < 0)
         {
             if (isRotatingAlways)
             {
-                transform.Rotate(0, 0, (0 - oneRotateDeg), Space.World);
+                float temp = oneRotateDeg * -1;
+                transform.Rotate(settedReverseAxis, Space.World);
             }
-            else if (maxRotateDeg != 0 || maxRotateVal != 0)
+            else if (maxRotateDeg != 0)
             {
-                if (transform.rotation.z > 0)
+                Debug.Log(settedEulerAxis);
+                if (settedEulerAxis > 1)
                 {
-
+                    transform.Rotate(settedReverseAxis, Space.World);
                 }
+            }
+            else if (maxRotateVal != 0 && currentRotateVal >= 0)
+            {
+                
+                if (settedEulerAxis > oneRotateDeg - 1)
+                {
+                    transform.Rotate(settedReverseAxis, Space.World);
+                }
+                else if(currentRotateVal > 0)
+                {
+                    transform.Rotate(settedReverseAxis, Space.World);
+                    currentRotateVal--;
+                }
+
             }
         }
     }
